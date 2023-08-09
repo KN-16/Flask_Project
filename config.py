264a8f1c08a31215fa80e9e3aa5293d99210bc29
KN161725 <137528188+KN161725@users.noreply.github.com>
2023-08-09 22:35:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 Server_name="LAPTOP-C6VJ3RCH\SQLEXPRESS"
 Db_name="KN16"
 Db_driver="ODBC+Driver+17+for+SQL+Server"
-SQLALCHEMY_DATABASE_URI=f"mssql+pyodbc://{Server_name}/{Db_name}?driver={Db_driver}"
+#SQLALCHEMY_DATABASE_URI=f"mssql+pyodbc://{Server_name}/{Db_name}?driver={Db_driver}"
+#SQLALCHEMY_DATABASE_URI="postgresql://kn16:W0BeMzCEoiKGX2zq8eQ8U9K4Hs9jnXeT@dpg-cj97cgavvtos73cff4jg-a.oregon-postgres.render.com/book_demo"
+SQLALCHEMY_DATABASE_URI="postgres://kn16:W0BeMzCEoiKGX2zq8eQ8U9K4Hs9jnXeT@dpg-cj97cgavvtos73cff4jg-a/book_demo"
 def Set_config(app):
-    app.config["SQLALCHEMY_DATABASE_URI"]=SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"]=SQLALCHEMY_DATABASE_URI
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
